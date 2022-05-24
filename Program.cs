1774a4f612d34307e00be08ab5182733b547bd9a
VictorDiaz <viktormdiaz.vd@gmail.com>
2022-05-24 17:50:03 -0500

    Actualizacion

--- Program.cs
+++ Program.cs
@@ -11,89 +11,95 @@ namespace DPRN2A2U3
         public static void Main()
         {
 
-            var lista = new List<string>();
-            List<String> variantesCovid = new List<String>();
-            variantesCovid.InsertRange(0, new String[] { "alfa", "beta", "gama", "delta", "omicron", "psi" });
-                              
-            int[] enteros4 = { 3, 6, 9, 12, 15, 18, 21 };
-            int[] enteros5 = new int[6];
-            int[] valores = null;
-            int[] dividendos = { 20, 7, 0 };
-        
+            try
+            {
+                var lista = new List<string>();
+                List<String> variantesCovid = new List<String>();
+                variantesCovid.InsertRange(0, new String[] { "alfa", "beta", "gama", "delta", "omicron", "psi" });
+
+                int[] enteros4 = { 3, 6, 9, 12, 15, 18, 21 };
+                int[] enteros5 = new int[6];
+                int[] valores = null;
+                int[] dividendos = { 20, 7, 0 };
+
                 Console.WriteLine("Número de elementos de lista: {0}", lista.Count);
-       
+
                 Console.WriteLine("El primer elemento de la lista es: '{0}'", lista[0]);
-          
+
 
                 for (int control = 0; control <= variantesCovid.Count; control++)
                     Console.Write("'{0}' ", variantesCovid[control]);
                 enteros5[enteros4.Length - 1] = enteros4[enteros4.Length - 1];
-                       
+
 
                 for (int i = 0; i <= 9; i++)
                     valores[i] = i * 2;
 
 
-            try
-            {
-
-
-
                 foreach (var value in valores)
-                    Console.WriteLine(value);          
+                    Console.WriteLine(value);
 
                 foreach (var value in dividendos)
-
 
                 {
                     Console.WriteLine("{0} dividido entre 2 es {1}", value, DivideEntreDos(value));
                 }
-                Console.WriteLine(Divide(1, 0));
 
             }
             catch (NullReferenceException ex)
             {
-                Console.WriteLine("");
+                Console.WriteLine(ex.Message);
+
             }
 
             catch (ArgumentOutOfRangeException ex)
             {
-                Console.WriteLine("");
+                Console.WriteLine(ex.Message);
             }
 
 
             catch (ArgumentException ex)
             {
-                Console.WriteLine("");
-            }
+                Console.WriteLine(ex.Message);
+            }          
 
-
-            //catch (ArgumentOutOfRangeException ex)
-            //{
-            //    Console.WriteLine("");
-            //}
-
-
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.WriteLine("");
-            }
-
-
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("");
-            }
+            Console.WriteLine(Divide(4, 2));
 
         }
 
         static int DivideEntreDos(int num)
-        {// Lanzar una excepción de argumento no válido si el número no es par.
-            return num / 2;
+        {
+            try
+            {
+
+                return num / 2;
+            }
+
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return num / 2;
+
+
+            }
+
         }
         static double Divide(int dividendo, int divisor)
         {
-            return dividendo / divisor;
+            try
+            {
+                return dividendo / divisor;
+
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return dividendo / divisor;
+
+            }
+
         }
 
     }
