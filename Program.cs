31dffdc4c36a1898f1ac91e0308a0d48d3e583ae
VictorDiaz <viktormdiaz.vd@gmail.com>
2022-06-11 15:47:24 -0500

    cambios

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace DPRN2A2U3
     internal class Program
     {
         public static void Main()
-        {                       
+        {      
+            try
+            { 
                 var lista = new List<string>();
                 List<String> variantesCovid = new List<String>();
                 variantesCovid.InsertRange(0, new String[] { "alfa", "beta", "gama", "delta", "omicron", "psi" });
@@ -17,7 +19,7 @@ namespace DPRN2A2U3
                 int[] enteros4 = { 3, 6, 9, 12, 15, 18, 21 };
                 int[] enteros5 = new int[6];
                 int[] valores = null;
-                int[] dividendos = { 20, 7, 0 };
+                int[] dividendos = { 20, 7, 0 };                
 
                 Console.WriteLine("Número de elementos de lista: {0}", lista.Count);
 
@@ -40,26 +42,54 @@ namespace DPRN2A2U3
                 foreach (var value in dividendos)
                 {
                     Console.WriteLine("{0} dividido entre 2 es {1}", value, DivideEntreDos(value));
-                }
+                }                
 
 
                 Console.WriteLine(Divide(1, 0));                         
+            }
+
+            catch (NullReferenceException ex)
+            {
+                Console.Write("Error : {0}", ex.Message.ToString());
+            }
+
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
             
+        static string DivideEntreDos(double num=0)
+        {
+            try
+            {
+                var resultado = num / 2;
+                return resultado.ToString();
+            }
+            catch (ArgumentException ex)
+            {
+                return "Error {0}" + ex.Message.ToString();
+            }
+
         }
 
-        static int DivideEntreDos(int num)
+        static string Divide(double dividendo=0, double divisor=0)
         {
-
-            return num / 2;
-
-        }
-
-        static double Divide(int dividendo, int divisor)
-        {
-
-            return dividendo / divisor;
-
-
+            try
+            {
+                var result =dividendo / divisor;
+                return result.ToString();
+            }
+            catch (DivideByZeroException ex)
+            {
+                return "Error {0}" + ex.Message.ToString();
+            }
         }
 
     }
